e860b1096350aa1f86031db995059b6de7f265f5
Nisa Doğan <nisadogn37@gmail.com>
2023-05-07 15:17:10 +0300

    baltanesi klasoru eklendi
    asdasdasdasdasd

---
+++ BalTanesi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalTanesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sizi çok seviyorum sayın Baltanesiii<3");
+
+            Console.Read();
+        }
+    }
+}
